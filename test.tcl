7ae5ca4e633cde3a0112d8e323fc01ab24b96c26
BCC Network <55225968+BCC-BGChatCommunity@users.noreply.github.com>
2022-12-15 12:02:07 +0200

    Update test.tcl

--- test.tcl
+++ test.tcl
@@ -1,7 +1,7 @@
 bind pub n|n github proc_rehash
 
 proc proc_rehash { nick uhost hand chan text } {
-  putquick "PRIVMSG $chan :github.com repository. Наздраве !!! и мезето и мезето!"
+  putquick "PRIVMSG $chan :github.com repository. Наздраве !!! и мезето и мезето тъничко нарязано...."
 }
 
 putlog "test.tcl is loaded from github.com repositiry"
