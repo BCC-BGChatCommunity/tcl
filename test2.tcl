7e9e1b3fbddc1db4a10b809947eb97966f4ae400
BGIRC Network <55225968+BCC-BGChatCommunity@users.noreply.github.com>
2022-12-15 15:32:15 +0200

    Update test2.tcl

--- test2.tcl
+++ test2.tcl
@@ -1,7 +1,7 @@
 bind pub n|n github2 proc_2
 
 proc proc_2 { nick uhost hand chan text } {
-  putquick "PRIVMSG $chan :github.com repository. 2"
+  putquick "PRIVMSG $chan :github.com repository. 22222222222222222"
 }
 
 putlog "test2.tcl is loaded from github.com repositiry"
